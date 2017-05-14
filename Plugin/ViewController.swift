38efec8753c3bafc1a8c81d4ca4380dd57fead9c
PEDRO ARMANDO MANFREDI <pedroarmandomanfredi@MacBook-Air-de-PEDRO.local>
2017-05-14 20:55:34 +0200

    Counters implemented

--- Plugin/ViewController.swift
+++ Plugin/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 import AVKit
 import AVFoundation
 
@@ -14,7 +15,13 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     
     var player: AVPlayer!
     var playerController: AVPlayerViewController!
-    
+    var play = false
+    var counterSeconds = 0.0
+    var counterPauses = 0
+    var counterPlays = 0
+    var timer = Timer()
+    var isTimerRunning = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -52,8 +59,22 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     
     //observer for av play
     override  func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "rate" {
-            print("rate")
+        if (keyPath == "rate" && play == true) {
+            play = !play
+            counterPlays += 1
+            print("PLAYING || PAUSE: \(counterPauses) times   ||   PLAY: \(counterPlays) times  || PAUSED TIME: \(timeString(time: counterSeconds)) ")
+            timer.invalidate()
+
+        }
+        else if (keyPath == "rate" && play == false){
+            counterPauses += 1
+            play = !play
+            print("PAUSE: \(counterPauses) times   ||   PLAY: \(counterPlays) times  || PAUSED TIME: \(timeString(time: counterSeconds)) ")
+
+            if isTimerRunning == false {
+                runTimer()
+            }
+            
         }
         
     
@@ -63,5 +84,26 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
         print("Video Finished")
     }
 
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    func updateTimer() {
+        counterSeconds += 0.1     //This will decrement(count down)the seconds.
+        print("\(counterSeconds)")
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        player.pause()
+        timer.invalidate()
+    }
 }
 
