382dbeaf15ec6d0c44d469ce1e7605c7f4c3b716
PEDRO ARMANDO MANFREDI <pedroarmandomanfredi@MacBook-Air-de-PEDRO.local>
2017-05-15 12:55:20 +0200

    done

--- Plugin/ViewController.swift
+++ Plugin/ViewController.swift
@@ -21,33 +21,37 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     var play = false
     var counterSeconds = 0.0
     var counterPauses = 0
-    var counterPlays = 0
+    var counterPlays = -1
     var timer = Timer()
-    var isTimerRunning = false
     var finalMessage: String = ""
-    let urlRequest = URL(string: "https://www.google.com")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.request()
+        
+        //test video link & player init
         let videoURL = URL(string: "http://techslides.com/demos/sample-videos/small.mp4")
-        
         player = AVPlayer(url:videoURL!)
-        
-        
         let playerController = AVPlayerViewController()
         playerController.player = player
         playerController.delegate = self
+        
+        //present playerController
         self.present(playerController, animated: true, completion: {
+            
+            //REQUEST:Video starts.
+            self.request()
+            
+            //autoplay at the start
             self.player.play()
+            
+            //add oberserver for rate... 0 pause/1 normal play
             self.player.addObserver(self, forKeyPath: "rate", options: .new, context: nil)
             NotificationCenter.default.addObserver(self, selector:#selector(ViewController.playerDidFinishPlaying(note:)),name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player.currentItem)
             
         } )
         
         
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
@@ -56,30 +60,31 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     }
     
     
-    
+    //dealloc rate
     func deallocObservers(player: AVPlayer) {
         player.removeObserver(self, forKeyPath: "rate")
         
         
     }
     
-    //observer for av play
+    //observer rate
     override  func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if (keyPath == "rate" && play == true) {
+            
+            //REQUEST: The user resumes the content.
+            self.request()
             play = !play
             counterPlays += 1
-            
             timer.invalidate()
             
         }
         else if (keyPath == "rate" && play == false){
+            
+            //The user pauses the content.
             counterPauses += 1
             play = !play
+            runTimer() // start timer
             
-            
-            if isTimerRunning == false {
-                runTimer()
-            }
             
         }
         
@@ -88,7 +93,12 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     
     func playerDidFinishPlaying(note: NSNotification) {
         print("Video Finished")
+        
+        //REQUEST: The video finishes.
+        self.request()
+        //-1 bc at the end rate change to 0,+1 in counter
         counterPauses -= 1
+        //dismiss video controller screen
         dismiss(animated: true, completion: nil)
     }
     
@@ -99,11 +109,11 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     
     
     func updateTimer() {
-        counterSeconds += 0.1     //This will decrement(count down)the seconds.
+        counterSeconds += 0.1     //This will increment the counterSeconds.
     }
     
     
-    func timeString(time:TimeInterval) -> String {
+    func timeString(time:TimeInterval) -> String { //counterSecond convert to h:m:s format
         let hours = Int(time) / 3600
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
@@ -111,12 +121,16 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     }
     
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) { //when end the video and get close
         
-        player.pause()
-        timer.invalidate()
+        player.pause() //important if the user click DONE
+        timer.invalidate() // stop timer
         finalMessage =  "PAUSE: \(counterPauses) times\nPLAY: \(counterPlays) times\nPAUSED TIME: \(timeString(time: counterSeconds))"
+        
+        //Dismiss screen
         playerController?.dismiss(animated: true, completion: nil)
+        
+        //create ALERT
         let alertController = UIAlertController(title: "FINISHED", message:"\(finalMessage)",preferredStyle: .alert)
         let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
         alertController.addAction(okAction)
@@ -126,7 +140,7 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     }
     
     
-    func request (){
+    func request (){ //request function askin to openweather api in real time the wheater.
         let parameters = ["q": "Barcelona", "APPID": "a51c02180847521c7f27d3ee317ec388"]
         //?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
         Alamofire.request("http://api.openweathermap.org/data/2.5/forecast", parameters: parameters) .responseJSON { response in
