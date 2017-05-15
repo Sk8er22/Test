09fa1719e948168c852320fdda9c94ad4bf66a77
PEDRO ARMANDO MANFREDI <pedroarmandomanfredi@MacBook-Air-de-PEDRO.local>
2017-05-15 10:39:15 +0200

    Finish

--- Plugin/ViewController.swift
+++ Plugin/ViewController.swift
@@ -21,39 +21,42 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     var counterPlays = 0
     var timer = Timer()
     var isTimerRunning = false
+    var finalMessage: String = ""
+    let urlRequest = URL(string: "https://www.google.com")
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            let videoURL = URL(string: "http://techslides.com/demos/sample-videos/small.mp4")
+        let videoURL = URL(string: "http://techslides.com/demos/sample-videos/small.mp4")
+        
+        player = AVPlayer(url:videoURL!)
+        
+        
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        playerController.delegate = self
+        self.present(playerController, animated: true, completion: {
+            self.player.play()
+            self.player.addObserver(self, forKeyPath: "rate", options: .new, context: nil)
+            NotificationCenter.default.addObserver(self, selector:#selector(ViewController.playerDidFinishPlaying(note:)),name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player.currentItem)
             
-            player = AVPlayer(url:videoURL!)
+        } )
         
-
-            let playerController = AVPlayerViewController()
-            playerController.player = player
-            playerController.delegate = self
-            self.present(playerController, animated: true, completion: {
-                self.player.play()
-                self.player.addObserver(self, forKeyPath: "rate", options: .new, context: nil)
-                NotificationCenter.default.addObserver(self, selector:#selector(ViewController.playerDidFinishPlaying(note:)),name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player.currentItem)
-
-            } )
-
         
-              // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     
     func deallocObservers(player: AVPlayer) {
         player.removeObserver(self, forKeyPath: "rate")
- 
+        
         
     }
     
@@ -62,48 +65,78 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
         if (keyPath == "rate" && play == true) {
             play = !play
             counterPlays += 1
-            print("PLAYING || PAUSE: \(counterPauses) times   ||   PLAY: \(counterPlays) times  || PAUSED TIME: \(timeString(time: counterSeconds)) ")
+            
             timer.invalidate()
-
+            
         }
         else if (keyPath == "rate" && play == false){
             counterPauses += 1
             play = !play
-            print("PAUSE: \(counterPauses) times   ||   PLAY: \(counterPlays) times  || PAUSED TIME: \(timeString(time: counterSeconds)) ")
-
+            
+            
             if isTimerRunning == false {
                 runTimer()
             }
             
         }
         
-    
+        
     }
     
     func playerDidFinishPlaying(note: NSNotification) {
         print("Video Finished")
+        dismiss(animated: true, completion: nil)
     }
-
+    
     
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
     }
     
+   
     func updateTimer() {
         counterSeconds += 0.1     //This will decrement(count down)the seconds.
-        print("\(counterSeconds)")
     }
     
+  
     func timeString(time:TimeInterval) -> String {
         let hours = Int(time) / 3600
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
         return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
     }
+   
+    
     override func viewDidAppear(_ animated: Bool) {
         
         player.pause()
         timer.invalidate()
+        finalMessage =  "PAUSE: \(counterPauses) times\nPLAY: \(counterPlays) times\nPAUSED TIME: \(timeString(time: counterSeconds))"
+        playerController?.dismiss(animated: true, completion: nil)
+        let alertController = UIAlertController(title: "FINISHED", message:"\(finalMessage)",preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+        print("\(finalMessage)")
+        
+    }
+    
+    
+    func httpRequest (){
+    _ = URLSession.shared.dataTask(with: urlRequest!) { data, response, error in
+    guard error == nil else {
+    print(error!)
+    return
+    }
+    guard let data = data else {
+    print("Data is empty")
+    return
+    }
+    
+    let json = try! JSONSerialization.jsonObject(with: data, options: [])
+    print(json)
+    }
+
     }
 }
 
