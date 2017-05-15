481939b71071182a8d15b6f352633c98a2c0474b
PEDRO ARMANDO MANFREDI <pedroarmandomanfredi@MacBook-Air-de-PEDRO.local>
2017-05-15 14:48:40 +0200

    All done

---
+++ Plugin/ControlPugin.swift
@@ -0,0 +1,13 @@
+//
+//  ControlPugin.swift
+//  Plugin
+//
+//  Created by PEDRO ARMANDO MANFREDI on 15/5/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import Foundation
+
+public class ControlPugin{
+    
+}
